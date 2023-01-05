56d4ff2012e55159f76dd0eea5b9e044e2f08475
Franklin da Silva Batista <frsbatist@gmail.com>
2023-01-04 21:59:47 -0300

    04/01/2023 21:58
    Modal de login e formulário de mensagens

--- assets/scripts.js
+++ assets/scripts.js
@@ -28,5 +28,18 @@ function subMenu3(){
 }
 btnMobile3.addEventListener('click', subMenu3);
 
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const  modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
 
+const toggleModal = () => {
+    modal.classList.toggle("hide");
+    fade.classList.toggle("hide");
+    
+};
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+    el.addEventListener("click", () => toggleModal());
+});
 
